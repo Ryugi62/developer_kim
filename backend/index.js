c507c000d2f338f78ad8328c4858019102f5ab1a
Ryugi62 <xorjf1027@naver.com>
2023-01-25 08:44:56 +0900

    vue history 추가로 새로고침시 오류가 생기는 부분 해결

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 // node express server for backend
 
+const history = require("connect-history-api-fallback");
 const express = require("express");
 const app = express();
 const port = 3000;
 
+// redirect all requests to index.html
+app.use(history());
 // public directory for static files (html, css, js, images)
 app.use(express.static("public"));
 
